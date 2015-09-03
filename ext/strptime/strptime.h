e4bd1b638ba92605d77c4ac74d8aed9ef40180ec
NARUSE, Yui <naruse@airemix.jp>
2015-09-04 03:42:53 +0900

    separeta MRI time related implementations

--- ext/strptime/strptime.h
+++ ext/strptime/strptime.h
@@ -2,6 +2,9 @@
 #define STRPTIME_H 1
 
 #include "ruby.h"
-VALUE timespec_new(const struct timespec *ts, int offset);
+VALUE rbtime_timespec_new(const struct timespec *ts, int offset);
+struct tm * localtime_with_gmtoff_zone(const time_t *t, struct tm *result, long *gmtoff, const char **zone);
+void timespec_now(struct timespec *ts);
+void tm_add_offset(struct tm *tm, long diff);
 
 #endif /* STRPTIME_H */
